473dbfa0c50057aebcf147543c0520ac908bf686
dipsyke <pogyornika@gmail.com>
2023-07-30 00:28:41 +0200

    added qr challenge

---
+++ Backend/2.3 NPM/index.js
@@ -0,0 +1,10 @@
+// var sillyName = generateName();
+//
+// import generateName from "sillyName";
+//
+// console.log(`My name is ${sillyName}`);
+
+import superheroes from "superheroes";
+superheroes.all;
+let superHeroName = superheroes.random();
+console.log(`I am ${superHeroName}`);
