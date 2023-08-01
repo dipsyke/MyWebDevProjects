e7a5cb2a0f7db6d3aeb33897c748d6e61765e2d5
dipsyke <pogyornika@gmail.com>
2023-08-02 01:50:27 +0200

    added band name generator

--- Backend/3.4 Middleware/index3.js
+++ Backend/3.4 Middleware/index3.js
@@ -3,12 +3,19 @@ import express from "express";
 const app = express();
 const port = 3004;
 
+
 app.use(logger);
 
 app.get("/", (req, res) => {
   res.send("Hello");
 });
 
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
+
+function logger (req, res, next) {
+  console.log("Request method: " + req.method + "; Request URL: " + req.url)
+  next();
+}
