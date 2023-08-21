16004aea28efa8976d82ce6f515a0bdd0103f639
dipsyke <pogyornika@gmail.com>
2023-08-21 20:21:12 +0200

    updated to do list

--- Backend/To Do List App/index.js
+++ Backend/To Do List App/index.js
@@ -6,41 +6,79 @@ const port = 3022;
 let workList = [];
 let todayList = [];
 
+let currentAutoIncrementValue = 1
+
+function getNewUniqueId() {
+    const valueToReturn = currentAutoIncrementValue
+    ++currentAutoIncrementValue
+    return valueToReturn
+}
+
+
 app.use(express.static("public"))
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.get("/", (req, res) => {
     res.render("index.ejs");
 })
 
-app.get("/work", (req, res)=>{
-    res.render("worklist.ejs", {worklist: workList});
+app.get("/work", (req, res) => {
+    res.render("taskList.ejs", {title: 'Work', taskList: workList});
 });
 app.post("/work", (req, res) => {
-   console.log(req.body)
-    let newItemWork = req.body.newItemWork;
-    if(newItemWork.length!==0) {
-        workList.push(newItemWork)
+    console.log(req.body)
+    let action = req.body.action;
+
+    if (action === "createNewTask") {
+        let newItemWork = req.body.newItem;
+        if (newItemWork.length !== 0) {
+            workList.push({
+                id: getNewUniqueId(),
+                name: newItemWork,
+                isDone: false
+            })
+        }
     }
 
-    res.render("worklist.ejs", {worklist: workList});
+    if (action === "setTaskState") {
+        let taskId = parseInt(req.body.taskId);
+        let newState = req.body.newState === "true";
+
+        let taskToModify = workList.find((element) => element.id === taskId)
+        taskToModify.isDone = newState
+    }
+    res.render("taskList.ejs", {title: 'Work', taskList: workList});
 })
 
 
-
-
-app.get("/today", (req, res)=>{
-    res.render("todaylist.ejs", {todaylist: todayList});
+app.get("/today", (req, res) => {
+    res.render("taskList.ejs", {title: 'Today', taskList: todayList});
 });
 app.post("/today", (req, res) => {
     console.log(req.body)
-    let newItemToday = req.body.newItemToday;
-    if(newItemToday.length!==0) {
-        todayList.push(newItemToday)
+    let action = req.body.action;
+
+    if (action === "createNewTask") {
+        let newItemToday = req.body.newItem;
+        if (newItemToday.length !== 0) {
+            todayList.push({
+                id: getNewUniqueId(),
+                name: newItemToday,
+                isDone: false
+            })
+        }
+    };
+
+    if (action === "setTaskState") {
+        let taskId = parseInt(req.body.taskId);
+        let newState = req.body.newState === "true";
+
+        let taskToModify = todayList.find((element) => element.id === taskId)
+        taskToModify.isDone = newState
     }
 
-    res.render("todaylist.ejs", {todaylist: todayList});
+    res.render("taskList.ejs", {title: 'Today', taskList: todayList});
 })
-app.listen(port, ()=> {
+app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
---
+++ Backend/To Do List App/public/script.js
@@ -0,0 +1,13 @@
+
+// let checkedItems = [];
+// let itemToCheck = $(".list-item")
+// itemToCheck.isChecked = false
+//
+// $(".list-item.checkbox").on("click"), function () {
+//     this.classList.toggle("checked");
+// if(this.checked){
+//     this.push(checkedItems);}
+// }
+
+
+
