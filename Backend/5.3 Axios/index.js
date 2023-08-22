d123cf9a597bd877340dfdce84749d2a79468ecf
dipsyke <pogyornika@gmail.com>
2023-08-23 00:57:38 +0200

    added axios module bored

--- Backend/5.3 Axios/index.js
+++ Backend/5.3 Axios/index.js
@@ -3,19 +3,17 @@ import bodyParser from "body-parser";
 import axios from "axios";
 
 const app = express();
-const port = 3000;
+const port = 3023;
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Step 1: Make sure that when a user visits the home page,
-//   it shows a random activity.You will need to check the format of the
-//   JSON data from response.data and edit the index.ejs file accordingly.
 app.get("/", async (req, res) => {
   try {
     const response = await axios.get("https://bored-api.appbrewery.com/random");
     const result = response.data;
-    res.render("index.ejs", { data: result });
+
+    res.render("index.ejs", { activityToDisplay: result });
   } catch (error) {
     console.error("Failed to make request:", error.message);
     res.render("index.ejs", {
@@ -26,15 +24,20 @@ app.get("/", async (req, res) => {
 
 app.post("/", async (req, res) => {
   console.log(req.body);
+  try {
+    let chosenType = req.body.type
+    let chosenParticipants = req.body.participants
+    const upstreamResponse = await axios.get(`https://bored-api.appbrewery.com/filter?type=${chosenType}&participants=${chosenParticipants}`);
+    console.log('response HERE:', upstreamResponse.data)
+    const result = upstreamResponse.data;
+    let randomChosenActivity = result[Math.floor(Math.random() * result.length)+1];
 
-  // Step 2: Play around with the drop downs and see what gets logged.
-  // Use axios to make an API request to the /filter endpoint. Making
-  // sure you're passing both the type and participants queries.
-  // Render the index.ejs file with a single *random* activity that comes back
-  // from the API request.
-  // Step 3: If you get a 404 error (resource not found) from the API request.
-  // Pass an error to the index.ejs to tell the user:
-  // "No activities that match your criteria."
+    res.render("index.ejs", { activityToDisplay: randomChosenActivity });
+  } catch (error) {
+    let errorMessage = "No activities that match your criteria.";
+    res.render("index.ejs", {error: errorMessage
+    });
+  }
 });
 
 app.listen(port, () => {
